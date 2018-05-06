52d716e0887fdde0fb1a57f295ccdcfee21a8e77
Nikita Katkov <n.katkov@inforion.ru>
2018-05-06 03:41:07 +0300

    tests added, small refactoring

--- src/test/java/ru/jetbrains/taskbalancer/BalancerTests.java
+++ src/test/java/ru/jetbrains/taskbalancer/BalancerTests.java
@@ -1,10 +1,23 @@
 package ru.jetbrains.taskbalancer;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import ru.jetbrains.taskbalancer.ownimlementation.threads.SimpleThread;
+import ru.jetbrains.taskbalancer.threads.SimpleThread;
+import ru.jetbrains.taskbalancer.utils.Balancer;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
 
 public class BalancerTests {
+    private Integer i1, i2, i3, i4, i5;
+    private Integer[] integerArray = {i1, i2, i3, i4, i5};
+
+    @BeforeClass
+    public static void init() {
+
+    }
 
     @Test
     public void testSingleThreadQueueWork() throws InterruptedException {
@@ -28,7 +41,22 @@ public class BalancerTests {
     }
 
     @Test
-    public void test() {
+    public void testRuntimeTasksSubmitting() throws InterruptedException {
+        Balancer balancer = new Balancer(1, 1);
+        new Thread(() -> balancer.setRunning(true).startBalancing()).start();
+
+        for (int index = 0; index < 5; index++) {
+            int finalIndex = index;
+            balancer.addTaskToQueue(() -> {
+                integerArray[finalIndex] = 1;
+                System.out.println(String.format("element %d updated", finalIndex));
+            });
+        }
+
+
+        Thread.sleep(10000L); // ожидание выполнения с запасом времени //todo заменить
+        balancer.setRunning(false);
+        Assert.assertTrue(Arrays.stream(integerArray).allMatch(Objects::nonNull));
 
     }
 }
